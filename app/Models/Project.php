d22ae2e25d0775f4d6521c52285a377128cbec80
Adam Kiss <iam@adamkiss.com>
2025-11-18 21:12:22 +0100

    Bootstrap Models

---
+++ app/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model {
+	/** @use HasFactory<\Database\Factories\ProjectFactory> */
+	use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'budget',
+        'company_id',
+    ];
+
+	public function company(): BelongsTo {
+		return $this->belongsTo(Company::class);
+	}
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,32 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    use WithoutModelEvents;
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void {
+		Company::factory(5)->create();
+		for ($i = 1; $i <= 5; $i++) {
+			Project::factory(4)->create([
+				'company_id' => $i,
+			]);
+		}
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+		for ($i=1; $i <= 20; $i++) {
+            $t = Task::factory()->create();
+            $t->created_at = now()
+                ->subHours(20 - $i)
+                ->subMinutes(rand(0,59))
+                ->subSeconds(rand(0,59));
+            $t->position = $i;
+            $t->save();
+        }
+	}
 }
